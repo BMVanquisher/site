73530a3fe09346e0a8afad2fe3b117e395c48af3
BMVanquisher <beschier94@gmail.com>
2016-03-15 11:35:34 +0200

    1

---
+++ WebSite8/app/components/app.config.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    "use strict";
+
+    angular.module('app')
+        .constant('appSettings', {
+            apiPath: 'http://localhost:50106/'
+        })
+        .config([
+            '$urlRouterProvider', '$stateProvider',
+            function($urlRouterProvider, $stateProvider) {
+                $urlRouterProvider.otherwise("/main");
+                $stateProvider
+                    .state("main", {
+                        url: "/main",
+                        templateUrl: "app/components/main.html",
+                        controller: "ModCtrl"
+
+                });
+            }
+        ]);
+}());
+                   
---
+++ WebSite8/app/components/mk.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('app').controller('ModCtrl', function ($scope, $modal) {
+        $scope.openModal = function () {
+            $modal.open({
+                templateUrl: 'app/components/modal.html ',
+                size: 'lg',
+                windowClass: 'full',
+                controller: function ($scope, $modalInstance) {
+                    $scope.closeForm = function () {
+                        $modalInstance.dismiss();
+                    };
+                }
+            });
+        };
+       
+    });
+})();
